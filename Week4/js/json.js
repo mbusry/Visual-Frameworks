ba7dd8c6121d1eaca57b15b61474257f89f0f428
mbusry <mbusry@fullsail.edu>
2013-02-25 14:33:13 -0500

    wk 4 v1
    Issues with browers

---
+++ Week4/js/json.js
@@ -0,0 +1,27 @@
+// Michael Usry
+// Project IV
+// Creating JSON data for testing
+
+
+var json = {
+    "item1" : {
+        "assigned"      : ["Assigned to:", "Mom"],
+        "email"         : ["eMail:", "test@me.com"],
+        "shop"          : ["Shop:", "Ingles"],
+        "when"          : ["When:", "2013-02-25"],
+        "qty"           : ["Quantity:", "2"],
+        "groceryItem"   : ["Grocery Item:", "meat"],
+        "notes"         : ["Notes:", "test 1"]
+        
+    },
+        "item1" : {
+        "assigned"      : ["Assigned to:", "Dad"],
+        "email"         : ["eMail:", "dad@me.com"],
+        "shop"          : ["Shop:", "Walmart"],
+        "when"          : ["When:", "2013-02-25"],
+        "qty"           : ["Quantity:", "2"],
+        "groceryItem"   : ["Grocery Item:", "snack"],
+        "notes"         : ["Notes:", "test 2"]
+
+    }
+}
